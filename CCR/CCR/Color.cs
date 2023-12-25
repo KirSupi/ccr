8a16a48aab032dd9ae9c11400a5f9482be31b9b4
kirichenko <@>
2023-12-25 21:26:34 +0300

    Color Class

--- CCR/CCR/Color.cs
+++ CCR/CCR/Color.cs
@@ -2,5 +2,15 @@ namespace CCR;
 
 public class Color
 {
-    
+    public Range[] HueRanges;
+    public Range[] SaturationRanges;
+    public Range[] BrightnessRanges;
+
+    public Color(Range[] hue, Range[] saturation, Range[] brightness)
+    {
+        // todo add params validation
+        HueRanges = hue;
+        SaturationRanges = saturation;
+        BrightnessRanges = brightness;
+    }
 }
