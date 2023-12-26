5c35092f619b790e56ac55c682daa889934c129e
kirichenko <@>
2023-12-26 12:59:04 +0300

    сделал валидацию для цветов

--- CCR/CCR/Color.cs
+++ CCR/CCR/Color.cs
@@ -8,62 +8,63 @@ public class Color
 
     public Color(Range[] hue, Range[] saturation, Range[] brightness)
     {
-        // todo add params validation
+        // validation
+        foreach (var range in hue)
+            if (range.Min < 0 || range.Max > 360)
+                throw new ArgumentException("hue cant be < 0 or > 360");
+        foreach (var range in saturation)
+            if (range.Min < 0 || range.Max > 100)
+                throw new ArgumentException("saturation cant be < 0 or > 100");
+        foreach (var range in brightness)
+            if (range.Min < 0 || range.Max > 100)
+                throw new ArgumentException("hue cant be < 0 or > 100");
+
         HueRanges = hue;
         SaturationRanges = saturation;
         BrightnessRanges = brightness;
     }
-    
+
     public bool Recognize(int hue, int saturation, int brightness)
     {
         var hueCollides = false;
-        
+
         foreach (var hueRange in HueRanges)
         {
-            if (hueRange.Includes(hue))
-            {
-                hueCollides = true;
-                break;
-            }
+            if (!hueRange.Includes(hue)) continue;
+
+            hueCollides = true;
+            break;
         }
 
-        if (!hueCollides)
-        {
-            return false;
-        }
-        
+        if (!hueCollides) return false;
+
+
         var saturationCollides = false;
-        
+
         foreach (var saturationRange in SaturationRanges)
         {
-            if (saturationRange.Includes(saturation))
-            {
-                saturationCollides = true;
-                break;
-            }
+            if (!saturationRange.Includes(saturation)) continue;
+
+            saturationCollides = true;
+            break;
         }
 
-        if (!saturationCollides)
-        {
-            return false;
-        }
-        
+        if (!saturationCollides) return false;
+
+
         var brightnessCollides = false;
-        
+
         foreach (var brightnessRange in BrightnessRanges)
         {
-            if (brightnessRange.Includes(brightness))
-            {
-                brightnessCollides = true;
-                break;
-            }
+            if (brightnessRange.Includes(brightness)) continue;
+
+            brightnessCollides = true;
+            break;
         }
 
-        if (!brightnessCollides)
-        {
-            return false;
-        }
-        
+        if (!brightnessCollides) return false;
+
+
         return true;
     }
 }
